8d50fc9a9a012cb0fd9ea9787303fdad61c223dd
Depleted <odepleted@gmail.com>
2024-12-19 18:54:59 +1300

    build: setup github pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.statalytics.net',
+	base: '/',
 	integrations: [
 		react(),
 		starlight({
